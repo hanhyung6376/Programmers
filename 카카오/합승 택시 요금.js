e92df30468c681ce08137493247f810a1170773d
hanhyung6376 <hanhyung6376@naver.com>
2021-11-05 16:22:20 +0900

    js 합승 택시 요금 - floyd warshall

--- 카카오/합승 택시 요금.js
+++ 카카오/합승 택시 요금.js
@@ -35,12 +35,3 @@ function solution(n, s, a, b, fares) {
     answer = Math.min(answer, graph[s][a] + graph[a][b], graph[s][b] + graph[b][a])
     return answer
 }
-
-
-n = 6;
-s = 4;
-a = 6;
-b = 2;
-fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]];
-
-console.log(solution(n, s, a, b, fares))
